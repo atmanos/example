d6eea23f7c4ffa1cbb53e1c35bcc9612d832a37c
Bernerd Schaefer <bj.schaefer@gmail.com>
2016-11-07 15:10:00 -0800

    netstack runs HTTP server with gonet

--- netstack/main.go
+++ netstack/main.go
@@ -1,102 +1,64 @@
-// Copyright 2016 The Netstack Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// This sample creates a stack with TCP and IPv4 protocols on top of a TUN
-// device, and listens on a port. Data received by the server in the accepted
-// connections is echoed back to the clients.
 package main
 
 import (
 	anet "atman/net"
 	"log"
-	"math/rand"
 	"net"
+	"net/http"
 	"os"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/google/netstack/tcpip"
-	"github.com/google/netstack/tcpip/link/sniffer"
+	"github.com/google/netstack/tcpip/adapters/gonet"
 	"github.com/google/netstack/tcpip/network/ipv4"
-	"github.com/google/netstack/tcpip/network/ipv6"
 	"github.com/google/netstack/tcpip/stack"
 	"github.com/google/netstack/tcpip/transport/tcp"
-	"github.com/google/netstack/waiter"
 )
 
-func init() {
-	log.SetOutput(os.Stdout)
-}
-
-func echo(wq *waiter.Queue, ep tcpip.Endpoint) {
-	defer ep.Close()
-
-	// Create wait queue entry that notifies a channel.
-	waitEntry, notifyCh := waiter.NewChannelEntry(nil)
-
-	wq.EventRegister(&waitEntry, waiter.EventIn)
-	defer wq.EventUnregister(&waitEntry)
-
-	for {
-		v, err := ep.Read(nil)
-		if err != nil {
-			if err == tcpip.ErrWouldBlock {
-				<-notifyCh
-				continue
-			}
-
-			return
-		}
-
-		ep.Write(v, nil)
-	}
-}
-
 func main() {
+	log.SetOutput(os.Stdout)
+
 	addrName := "10.0.2.20"
-	portName := "9999"
+	port := 9999
 
-	rand.Seed(time.Now().UnixNano())
+	addr := tcpip.Address(net.ParseIP(addrName).To4())
 
-	// Parse the IP address. Support both ipv4 and ipv6.
-	parsedAddr := net.ParseIP(addrName)
-	if parsedAddr == nil {
-		log.Fatalf("Bad IP address: %v", addrName)
-	}
-
-	var addr tcpip.Address
-	var proto tcpip.NetworkProtocolNumber
-	if parsedAddr.To4() != nil {
-		addr = tcpip.Address(parsedAddr.To4())
-		proto = ipv4.ProtocolNumber
-	} else if parsedAddr.To16() != nil {
-		addr = tcpip.Address(parsedAddr.To16())
-		proto = ipv6.ProtocolNumber
-	} else {
-		log.Fatalf("Unknown IP type: %v", addrName)
-	}
-
-	localPort, err := strconv.Atoi(portName)
+	s, err := newStack(addr)
 	if err != nil {
-		log.Fatalf("Unable to convert port %v: %v", portName, err)
+		log.Fatal(err)
 	}
 
-	// Create the stack with ip and tcp protocols, then add a tun-based
-	// NIC and address.
-	s := stack.New([]string{ipv4.ProtocolName, ipv6.ProtocolName, "arp"}, []string{tcp.ProtocolName})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("A very HTTP-hello from AtmanOS\n"))
+	})
+
+	listenAndServe(s, addr, port)
+}
+
+func listenAndServe(s tcpip.Stack, addr tcpip.Address, port int) {
+	fullAddr := tcpip.FullAddress{
+		NIC:  1,
+		Addr: addr,
+		Port: uint16(port),
+	}
+
+	ln, _ := gonet.NewListener(s, fullAddr, ipv4.ProtocolNumber)
+
+	http.Serve(ln, nil)
+}
+
+func newStack(addr tcpip.Address) (tcpip.Stack, error) {
+	s := stack.New([]string{ipv4.ProtocolName}, []string{tcp.ProtocolName})
 
 	mac := parseHardwareAddr(string(anet.DefaultDevice.MacAddr))
 	linkID := stack.RegisterLinkEndpoint(NewEtharpLink(mac, addr, &netif{anet.DefaultDevice}))
-	sniffed := sniffer.New(linkID)
 
-	if err := s.CreateNIC(1, sniffed); err != nil {
-		log.Fatal(err)
+	if err := s.CreateNIC(1, linkID); err != nil {
+		return nil, err
 	}
 
-	if err := s.AddAddress(1, proto, addr); err != nil {
-		log.Fatal(err)
+	if err := s.AddAddress(1, ipv4.ProtocolNumber, addr); err != nil {
+		return nil, err
 	}
 
 	// Add default route.
@@ -109,41 +71,5 @@ func main() {
 		},
 	})
 
-	// Create TCP endpoint, bind it, then start listening.
-	var wq waiter.Queue
-	ep, err := s.NewEndpoint(tcp.ProtocolNumber, proto, &wq)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer ep.Close()
-
-	if err := ep.Bind(tcpip.FullAddress{0, "", uint16(localPort)}, nil); err != nil {
-		log.Fatal("Bind failed: ", err)
-	}
-
-	if err := ep.Listen(10); err != nil {
-		log.Fatal("Listen failed: ", err)
-	}
-
-	// Wait for connections to appear.
-	waitEntry, notifyCh := waiter.NewChannelEntry(nil)
-	wq.EventRegister(&waitEntry, waiter.EventIn)
-	defer wq.EventUnregister(&waitEntry)
-
-	for {
-		log.Println("accepting")
-
-		n, wq, err := ep.Accept()
-		if err != nil {
-			if err == tcpip.ErrWouldBlock {
-				<-notifyCh
-				continue
-			}
-
-			log.Fatal("Accept() failed:", err)
-		}
-
-		go echo(wq, n)
-	}
+	return s, nil
 }
